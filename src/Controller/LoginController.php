271369d45c85d51334ad384847b5c4018866633f
Zied310 <ziedzhiri@yahoo.fr>
2024-03-22 11:44:23 +0100

    basic login

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -8,44 +8,48 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
+
 class LoginController extends AbstractController
 {
-    #[Route('/login', name: 'app_login')]
-    public function login(Request $request): Response
-    {   
-        $error = '';
-                // Check if the form is submitted
-                if ($request->isMethod('POST')) {
-                    // Retrieve email and password from the request
-                    $email = $request->request->get('email');
-                    $password = $request->request->get('password');
-        
-                    // Retrieve user by email from the database
-                    $userRepository = $this->getDoctrine()->getRepository(Users::class);
-                    $user = $userRepository->findOneBy(['email' => $email]);
-        
-                    // If user exists and password matches
-                    if ($user && $user->getMdpUser() === $password) {
-                        // Log in the user
-                        // You can store user information in session or cookie
-                        // For example, $this->session->set('user', $user);
-                        
-                        // Redirect to a secured page
-                        return $this->redirectToRoute('secured_page');
-                    } else {
-                        // Authentication failed, handle error
-                        // For example, display error message
-                        $error = 'Invalid email or password';
 
-                    }
-                }
-        
-                // Render login form
-                return $this->render('login/login.html.twig', [
-                    'error' => $error, // Pass the error variable to the template
-                ]);
+    #[Route('/login', name: 'app_login', methods:['POST'])]
+    public function login(Request $request): Response
+    {
+        $error = '';
+        $success = '';
+
+        $email = $request->request->get('_email');
+        $password = $request->request->get('_password');
+
+        $userRepository = $this->getDoctrine()->getRepository(Users::class);
+        $user = $userRepository->findOneBy(['email' => $email]);
+
+        if ($user && $user->getMdpUser() === $password) {
+            $success= 'Login successful';
+
+        } else {
+            $error = 'Invalid email or password';
+
+        }
+
+        return $this->render('login/login.html.twig', [
+            'error'         => $error,
+            'success'       => $success,
+        ]);
+
+    }
+    #[Route('/login_check', name: 'app_login_check')]
+    public function loginCheck()
+    {
+        // This code will not be executed
     }
 
+    #[Route('/logout', name: 'app_logout')]
+    public function logout()
+    {
+        // This code will not be executed
+    }
 
 }
 
