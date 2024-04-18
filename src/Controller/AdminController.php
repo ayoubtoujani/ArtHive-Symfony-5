cb988c48d8c1c28dcadf4cfebc4ace0ce12db209
Zied310 <ziedzhiri@yahoo.fr>
2024-04-18 22:39:50 +0100

    profile edit fix, added CRUD for users pages in admin dashboard(no password editing)

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -1,10 +1,12 @@
 <?php
 
 namespace App\Controller;
-
+use App\Entity\Users;
+use App\Form\EditUserFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AdminController extends AbstractController
@@ -20,10 +22,54 @@ class AdminController extends AbstractController
     #[Route('/admin/users', name: 'app_admin_users', methods:['GET'])]
     public function getAllUsers()
     {
+        $userRepository = $this->getDoctrine()->getRepository(Users::class);
+        $users= $userRepository->findAll();
+
         return $this->render('admin/users.html.twig', [
+            'users' => $users,
             'controller_name' => 'AdminController',
         ]);
     }
+
+    #[Route('/admin/user/edit/{id}', name: 'app_admin_user_edit', methods:['POST'])]
+    public function edit(Request $request, $id)
+    {
+        $userRepository = $this->getDoctrine()->getRepository(Users::class);
+        $user = $userRepository->find($id);
+        if (!$user) {
+            throw $this->createNotFoundException('No user found for id '.$id);
+        }
+
+        $form = $this->createForm(EditUserFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+            return $this->redirectToRoute('app_admin_users');
+        }
+
+        return $this->render('admin/user-edit.html.twig', [
+            'user' => $user,
+            'registerForm' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/admin/user/delete/{id}', name: 'app_admin_user_delete', methods:['POST'])]
+    public function delete($id)
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $userRepository = $entityManager->getRepository(Users::class);
+        $user = $userRepository->find($id);
+        if (!$user) {
+            throw $this->createNotFoundException('No user found for id '.$id);
+        }
+
+        $entityManager->remove($user);
+        $entityManager->flush();
+        return $this->redirectToRoute('app_admin_users');
+    }
+
+
 
     #[Route('/admin/groups', name: 'app_admin_users', methods:['GET'])]
     public function getAllGroups()
---
+++ src/Form/EditUserFormType.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Users;
+use DateTime;
+use App\Services\CountryApi;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Validator\Constraints\Regex;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\Email;
+use App\Validator\Constraints\UniqueEmail;
+use Symfony\Component\Validator\Constraints\Date;
+use Symfony\Component\Validator\Constraints\Callback;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
+
+class EditUserFormType extends AbstractType
+{
+    private $countryApi;
+
+    public function __construct(CountryApi $countryApi)
+    {
+        $this->countryApi = $countryApi;
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+        ->add('nomUser', TextType::class, [
+            'constraints' => [
+                new NotBlank(['message' => 'Please enter your name.']),
+                new Regex([
+                    'pattern' => '/^\D+$/',
+                    'message' => 'Your name cannot contain numbers.',
+                ]),
+            ],
+        ])
+
+        ->add('prenomUser', TextType::class, [
+            'constraints' => [
+                new NotBlank(['message' => 'Please enter your surname.']),
+                new Regex([
+                    'pattern' => '/^\D+$/',
+                    'message' => 'Your surname cannot contain numbers.',
+                ]),
+            ],
+        ])
+
+        ->add('email', EmailType::class, [
+            'constraints' => [
+                new NotBlank(['message' => 'Please enter your email.']),
+                new Email(['message' => 'The email "{{ value }}" is not a valid email.']),
+                new UniqueEmail(),
+            ],
+        ])
+
+        ->add('dNaissanceUser', DateType::class, [
+            'widget' => 'single_text',
+            'constraints' => [
+                new NotBlank(['message' => 'Please enter your date of birth.']),
+                new Callback([$this, 'validateAge']),
+            ],
+        ])
+
+        ->add('ville', ChoiceType::class, [
+            'choices' => $this->getCountryChoices(),
+            'constraints' => [
+                new NotBlank(['message' => 'Please select your country.']),
+            ],
+        ])
+
+        ->add('numTelUser', TelType::class, [
+            'constraints' => [
+                new NotBlank(['message' => 'Please enter your phone number.']),
+                new Regex([
+                    'pattern' => '/^\d+$/',
+                    'message' => 'Your phone number should contain only numbers.',
+                ]),
+            ],
+        ]);
+    }
+
+    public function validateAge($value, ExecutionContextInterface $context)
+    {
+        $today = new \DateTime();
+        $age = $today->diff($value)->y;
+        if ($age < 18) {
+            $context->buildViolation('You must be at least 18 years old.')->addViolation();
+        }
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Users::class,
+        ]);
+    }
+
+    private function getCountryChoices(): array
+    {
+        $countries =  $this->countryApi->getCountries();
+
+        $choices = ['Countries' => ''];
+        foreach ($countries as $country) {
+            $choices[$country] = $country;
+        }
+        asort($choices);
+        return $choices;
+    }
+}
