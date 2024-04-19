ac266cff0bf3d21008f93daac66fef22fb57dfcb
shams <chamsal-islem.bensaid@esprit.tn>
2024-04-19 01:06:39 +0100

    Ajouter Produit Form

--- src/Form/ProduitType.php
+++ src/Form/ProduitType.php
@@ -14,8 +14,12 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\RangeType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-
 use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+
 class ProduitType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
@@ -29,34 +33,77 @@ class ProduitType extends AbstractType
             'attr' => [
                 'class' => 'upload-button',
                 'accept' => 'image/*', // Accept only image files
-            ],
+            ], 
+            'constraints' => [
+                new Assert\NotBlank([
+                    'message' => 'Veuillez sélectionner une image.',
+                ]),
+            ],      
         ])
 
         ->add('nomProduit', TextType::class, [
-            'label' => 'Nom du produit'
+            'label' => 'Nom du produit',
+            'constraints' => [
+                new Assert\NotBlank([
+                    'message' => 'Le nom du produit ne peut pas être vide.',
+                ]),
+                new Assert\Length([
+                    'min' => 3,
+                    'minMessage' => 'Le nom du produit doit contenir au moins {{ limit }} caractères.',
+                ]),
+            ],    
         ])
 
       
-        ->add('prixProduit', IntegerType::class, [
+        ->add('prixProduit', NumberType::class, [
             'label' => 'Prix du produit',
+            'constraints' => [
+                new Assert\NotBlank([
+                    'message' => 'Le prix du produit ne peut pas être vide.',
+                ]),
+            ],
             'attr' => [
                 'min' => 0,
                 'max' => 100000, 
-            ]
+                'step' => 0.1,
+                'value' => 0,
+                
+            ],
+            'html5' => true,
         ])
         ->add('descriptionProduit', TextareaType::class, [
-            'label' => 'Description du produit'
+            'label' => 'Description du produit',
+            'constraints' => [
+                new Assert\NotBlank([
+                    'message' => 'La description du produit ne peut pas être vide.',
+                ]),
+                new Assert\Length([
+                    'min' => 3,
+                    'minMessage' => 'La description doit contenir au moins {{ limit }} caractères.',
+                ]),
+            ],    
         ])
         ->add('disponibilite', CheckboxType::class, [
             'label' => 'Disponibilité du produit',
             'required' => false,
+            'attr' => [
+                'id' => 'disponibilite-checkbox', // Ajout de l'ID
+            ]
+           
         ])
         ->add('stockProduit', IntegerType::class, [
             'label' => 'Stock du produit',
             'attr' => [
                 'min' => 0,
-                'max' => 1000, 
-            ]
+                'max' => 100000, 
+                'value' => 0,
+                'id' => 'stock-field',
+            ],
+            'constraints' => [
+                new Assert\NotBlank([
+                    'message' => 'Le prix du produit ne peut pas être vide.',
+                ]),
+            ],
         ])
         ->add('categProduit', ChoiceType::class, [
             'label' => 'Catégorie du produit',
@@ -73,6 +120,11 @@ class ProduitType extends AbstractType
                 'Illustration' => 'ILLUSTRATION',
                 'Autre' => 'AUTRE',
             ],
+                'constraints' => [
+                    new Assert\NotBlank([
+                        'message' => 'Veuillez sélectionner une catégorie.',
+                    ]),
+                ],
         ]);
     }
 
@@ -80,6 +132,7 @@ class ProduitType extends AbstractType
     {
         $resolver->setDefaults([
             'data_class' => Produits::class,
+            'imageFileName' => null, 
         ]);
     }
 }
