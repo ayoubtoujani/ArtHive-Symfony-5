8364bdd241eb8125cc289cc3393468c8eb96188a
toujaniayoub <ayoub.toujani@esprit.tn>
2024-04-27 16:02:24 +0200

    fixing the share on facebook

--- src/Controller/CommentsController.php
+++ src/Controller/CommentsController.php
@@ -240,17 +240,4 @@ public function addReaction($id, Request $request, SessionInterface $session): R
         return $this->redirectToRoute('app_login');
     }
 }
-
-#[Route('/share-on-facebook', name: 'share_on_facebook')]
-public function shareOnFacebook(Request $request, Facebook $facebook): Response
-{
-    // Get the URL of the current page
-    $url = $request->getUri();
-
-    // Create a Facebook sharing link with the URL
-    $link = 'https://www.facebook.com/sharer/sharer.php?u=' . urlencode($url);
-
-    // Redirect the user to the Facebook sharing dialog
-    return new RedirectResponse($link);
-}
 }
